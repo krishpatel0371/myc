76ccc98b4d367ea08cb5c2a1b5a90060b192cec8
krishpatel <140943044+krishpatel0371@users.noreply.github.com>
2023-09-14 08:51:00 +0530

    Add files via upload

---
+++ lab6 A 4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	printf("enter your any number:-");
+	scanf("%d",&x);
+	if (x%2==0)
+	{
+		printf(" the last digit is even.");
+	}
+	else
+	{
+		printf(" the last digit is odd. ");
+	}
+	
+}
+
