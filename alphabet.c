b9f5d5de6017310b4570a2214ba411c182b32403
krishpatel <140943044+krishpatel0371@users.noreply.github.com>
2023-09-19 13:24:35 +0530

    Add files via upload

---
+++ alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+ 
+   char alphabet; 
+   printf("enter any value");
+   scanf("%c",&alphabet);
+   alphabet>='a' && alphabet<='z' || alphabet>='A' && alphabet<='Z'? printf("alphabet"):printf("not alphabet");
+
+	
+	
+	
+}
---
+++ mult.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main(){
+	int a,b,c,max,ans;
+	printf("enetr the no :");
+	scanf("%d %d %d",&a,&b,&c);
+	max=a>b? a :b;
+    switch(max){
+    	default:
+    		printf("result is %d",max*c);
+	}
+	
+	
+	
+}
